c5b564b78edb796d3fb74dc5a7fadd83558eddbf
tizee <33030965+tizee@users.noreply.github.com>
2022-04-28 01:39:58 +0800

    init

---
+++ Formula/plan9port.rb
@@ -0,0 +1,43 @@
+# coding: utf-8
+class Plan9Port < Formula
+  desc "Plan 9 from User Space (aka plan9port) is a port of many Plan 9 programs from their native Plan 9 environment to Unix-like operating systems. "
+  homepage "https://9fans.github.io/plan9port/"
+  head "https://github.com/9fans/plan9port.git", branch: "master"
+  url "https://github.com/9fans/plan9port/archive/master.zip"
+  keg_only "Plan9Port includes Unix system programs may override their OS X equivalents."
+
+  def install
+    # 1. build with plan9port script
+    # 2. symlink 9 to /usr/local/bin/9
+    ENV["PLAN9_TARGET"] = libexec
+
+    system "./INSTALL"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/9"
+    prefix.install Dir[libexec/"mac/*.app"]
+  end
+
+  def caveats; <<~EOS
+    In order not to collide with macOS system binaries, the Plan 9 binaries have
+    been installed to #{opt_libexec}/bin.
+    To run the Plan 9 version of a command simply call it through the command
+    "9", which has been installed into the Homebrew prefix bin.  For example,
+    to run Plan 9's ls run:
+        # 9 ls
+  EOS
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <u.h>
+      #include <libc.h>
+      #include <stdio.h>
+      int main(void) {
+        return printf("Hello World\\n");
+      }
+    EOS
+    system bin/"9", "9c", "test.c"
+    system bin/"9", "9l", "-o", "test", "test.o"
+    assert_equal "Hello World\n", shell_output("./test", 1)
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# plan9port-rb
+
+Why? The `homebrew-core` has removed `plan9port` (see [brew#6478] and [homebrew-core#38394]). So we cannot install with `brew install plan9port` anymore.
+
+[brew#6478](https://github.com/Homebrew/brew/issues/6478)
+[homebrew-core#38394](https://github.com/Homebrew/homebrew-core/pull/38394)
+
+## Usage
+
+```zsh
+brew tap tizee/plan9port-rb
+brew install plan9port
+```
